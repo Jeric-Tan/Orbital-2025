d7329716bcf81888fdfef700805c66900bfd3f0f
Jeric Tan <2003jeric@gmail.com>
2025-03-22 04:06:19 +0800

    +app +Login page

--- Lyst/app/App.tsx
+++ Lyst/app/App.tsx
@@ -1,7 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Login from './app/screens/Login';
-
+import Login from './screens/Login';
 
 const Stack = createNativeStackNavigator();
 export default function App() {
